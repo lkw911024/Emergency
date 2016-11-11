5b5c9806a3253aaf11e7980d43035d471c274374
Huni <zxca1520@gmail.com>
2016-11-11 22:56:59 +0900

    start emergency project

--- app/src/main/java/com/emergency/lecture/emergency/SignUp.java
+++ app/src/main/java/com/emergency/lecture/emergency/SignUp.java
@@ -1,13 +1,25 @@
 package com.emergency.lecture.emergency;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
 
 public class SignUp extends AppCompatActivity {
+
+    private EditText id;
+    private EditText password;
+    private EditText passwordCheck;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
     }
+    public void onClickSignUpBtn(View v)
+    {
+        System.out.println("등록을 눌렀습니다.");
+    }
+
 }
